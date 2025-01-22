402c645342940f784bd311c12330c7d9be18cac0
mateus1020 <mateusd823@gmail.com>
2025-01-21 23:52:04 -0300

    Projeto Heel

---
+++ script.js
@@ -0,0 +1,26 @@
+const elementos = document.querySelectorAll(".animado");
+// Animação dos elementos na pagina.
+window.addEventListener("scroll", () => {
+    const trigger = window.innerHeight * 0.7;
+
+    elementos.forEach((el) => {
+        const pos = el.getBoundingClientRect().top;
+        if (pos < trigger) el.classList.add("visivel");
+    });
+});
+
+// Função para voltar ao topo
+const botao = document.getElementById('voltar-ao-topo');
+function voltarAoTopo() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+botao.addEventListener('click', voltarAoTopo);
+// Mostra o botão apenas quando o usuário scrollar
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 500) { // Mostra após 500px de scroll
+    botao.style.display = 'block';
+  } else {
+    botao.style.display = 'none';
+  }
+});
+
